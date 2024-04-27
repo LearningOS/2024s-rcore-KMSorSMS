d173d0520b109b6d968096804d3669888563b286
liam <liamyzw@126.com>
2024-04-27 19:21:33 +0800

    fin lab1

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,7 +1,8 @@
 //! Process management syscalls
 use crate::{
     config::MAX_SYSCALL_NUM,
-    task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus},
+    syscall::TASK_INFO_LIST,
+    task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus, TASK_MANAGER},
     timer::get_time_us,
 };
 
@@ -14,6 +15,7 @@ pub struct TimeVal {
 
 /// Task information
 #[allow(dead_code)]
+#[derive(Copy, Clone)]
 pub struct TaskInfo {
     /// Task status in it's life cycle
     status: TaskStatus,
@@ -21,6 +23,25 @@ pub struct TaskInfo {
     syscall_times: [u32; MAX_SYSCALL_NUM],
     /// Total running time of task
     time: usize,
+}
+
+impl TaskInfo {
+    pub fn new() -> Self {
+        TaskInfo {
+            status: TaskStatus::Ready,
+            syscall_times: [0; MAX_SYSCALL_NUM],
+            time: 0,
+        }
+    }
+    pub fn set_status(&mut self, status: TaskStatus) {
+        self.status = status;
+    }
+    pub fn add_syscall_time(&mut self, syscall: usize) {
+        self.syscall_times[syscall] += 1;
+    }
+    pub fn set_time(&mut self, task_start: usize, task_syscall: usize) {
+        self.time = task_syscall - task_start;
+    }
 }
 
 /// task exits and submit an exit code
@@ -53,5 +74,16 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 /// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info");
-    -1
+    // 取出当前任务的taskinfo，然后一个个传给指针
+    let current = TASK_MANAGER.get_current_task();
+    let task_info_list = TASK_INFO_LIST.exclusive_access();
+    let current_task_info = &task_info_list[current];
+    // 把参数给到指针指向的空间
+    unsafe{
+        (*_ti).time = current_task_info.time;
+        (*_ti).syscall_times = current_task_info.syscall_times;
+        (*_ti).status = current_task_info.status;
+    }
+    drop(task_info_list);
+    0
 }
